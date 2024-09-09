128020aada5a05de41cda67830a43f2005abc199
Marc Prud'hommeaux <marc@skip.tools>
2024-09-09 16:03:16 -0400

    Add addition termux packages to support libcurl and libxml

--- get-packages-and-swift-source.swift
+++ get-packages-and-swift-source.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 // The Termux packages to download and unpack
-var termuxPackages = ["libicu", "libicu-static", "libandroid-spawn", "libcurl", "libxml2", "libnghttp3"]
+// libxml2 needs liblzma and libiconv
+// libcurl needs zlib, libnghttp3, libnghttp2, libssh2, and openssl
+var termuxPackages = ["libicu", "libicu-static", "libandroid-spawn", "libcurl", "zlib", "libxml2", "libnghttp3", "libnghttp2", "libssh2", "openssl", "liblzma", "libiconv"]
 let termuxURL = "https://packages.termux.dev/apt/termux-main"
 
 let swiftRepos = ["llvm-project", "swift", "swift-experimental-string-processing", "swift-corelibs-libdispatch",
