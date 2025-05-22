0422744cd7b467628cc6fc5eaccf666c6a18b29e
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-05-21 20:15:59 -0400

    Update apply-patches

--- apply-patches.sh
+++ apply-patches.sh
@@ -18,12 +18,13 @@ case "${BUILD_SCHEME}" in
         ;;
     swift-*-branch)
         git apply -v -C1 ${patches_dir}/swift-android.patch
+        git apply -v -C1 ${patches_dir}/swift-android-devel.patch
         git apply -v -C1 ${patches_dir}/swift-android-testing-except-release.patch
         ;;
     development)
         git apply -v -C1 ${patches_dir}/swift-android.patch
-        git apply -v -C1 ${patches_dir}/swift-android-testing-except-release.patch
         git apply -v -C1 ${patches_dir}/swift-android-trunk-libdispatch.patch
+        git apply -v -C1 ${patches_dir}/swift-android-testing-except-release.patch
         ;;
     *)
         echo "$0: invalid BUILD_SCHEME=${BUILD_SCHEME}"
